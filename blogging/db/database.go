f4a80659b5a073fab47bc084075ca4700908321f
Maniram Yadav <26573822+maniram-yadav@users.noreply.github.com>
2025-02-10 17:19:54 +0530

    database model added

---
+++ blogging/db/database.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"log"
+	"sync"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	db   *gorm.DB
+	once sync.Once
+)
+
+func initDB() *gorm.DB {
+	dsn := "host=localhost user=postgres password=postgres dbname=blogging port=5000 sslmode=disable TimeZone=Asia/Shanghai"
+	var err error
+
+	once.Do(func() {
+		// db, err = gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	})
+	if err != nil {
+		log.Fatal("failed to connec with DB", err)
+	}
+	db.AutoMigrate(&mode.User{}, &model.Blog{}, &model.Comment{}, &model.Subscription{})
+	return db
+}
