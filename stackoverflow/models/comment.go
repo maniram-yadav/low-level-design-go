95d7f022ebc285a505c4a2b31fec490ffa3f5ee4
maniram <maniramyadav786@gmail.com>
2025-04-08 13:55:20 +0000

    question answer struct added

--- stackoverflow/models/comment.go
+++ stackoverflow/models/comment.go
@@ -1,4 +1,13 @@
 package models
 
+import "time"
+
 type Comment struct {
+	ID         uint      `json:"id"`
+	Text       string    `json:"text"`
+	UserID     uint      `json:"userId"`
+	User       User      `json:"user,omitempty"`
+	CreatedAt  time.Time `json:"createdAt"`
+	ParentID   uint      `json:"parentId"`
+	ParentType string    `json:"parentType"`
 }
--- stackoverflow/models/question.go
+++ stackoverflow/models/question.go
@@ -1,4 +1,18 @@
 package models
 
+import "time"
+
 type Question struct {
+	ID        uint      `json:"id"`
+	Title     string    `json:"title"`
+	Body      string    `json:"body"`
+	UserID    uint      `json:"userId"`
+	User      User      `json:"user,omitempty"`
+	Answers   []Answer  `json:"answers,omitempty"`
+	Tags      []Tag     `json:"tags,omitempty" gorm:"many2many:question_tags;"`
+	VoteCount int       `json:"voteCount"`
+	Views     int       `json:"views"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	Status    string    `json:"status"`
 }
