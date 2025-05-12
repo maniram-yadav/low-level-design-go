0e022c50028c19dbfc3e5fe9f83486edaa86b286
maniram <maniramyadav786@gmail.com>
2025-05-12 14:52:48 +0000

    add contact implementation

--- truecaller/truecallerservice.go
+++ truecaller/truecallerservice.go
@@ -1,6 +1,9 @@
 package truecaller
 
-import "sync"
+import (
+	"context"
+	"sync"
+)
 
 type TrueCallerService struct {
 	db              Database
@@ -10,4 +13,37 @@ type TrueCallerService struct {
 	rateLimiter     *RateLimiter
 	carrierService  CarrierService
 	locationService LocationService
+}
+
+
+func (t *TrueCallerService) AddContact(ctx context.Context,userId string,contact *Contact) error {
+	user,err := t.getUser(ctx,userId)
+	if err != nil {
+		return err
+	}
+	user.Lock()
+	defer user.Unlock()
+
+	for _,phone := contact.PhoneNumbers {
+		if _,exists := user.Contacts[phone.String()];exists {
+			return ErrContactExists
+		}
+	}
+	for _,phone := range contact.PhoneNumbers {
+		 user.Contacts[phone.String()] = contact
+	}
+	return t.db.SaveUser(ctx,user)
+}
+
+
+func (t *TrueCallerService) getUser(ctx context.Context, userID string) (*User, error) {
+	if cached, ok := t.userCache.Load(userID); ok {
+		return cached.(*User), nil
+	}
+	user, err := t.db.GetUserByID(ctx, userID)
+	if err != nil {
+		return nil, err
+	}
+	t.userCache.Store(userID, user)
+	return user, nil
 }
